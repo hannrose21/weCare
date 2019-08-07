3a963ea8202c2be7b46dd7641e333e8804ffc323
Hannah Williams <hannah_W@RBCCM-04s-MacBook-Air.local>
2019-08-07 14:24:19 -0400

    links,buttons and linking other pages into the home page

---
+++ weCare.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+function myMenu(){
+  document.getElementById("drop").classList.toggle("show");
+}
+
+//function shrink(x) {
+  //console.log("print");
+  //document.getElementById("skin").classList.toggle("hide");
+//}
+
+//function myHealth(){
+  //document.getElementById("health").classList.toggle("show");
+//}
+
+function myBeauty2(){
+  document.getElementById("both").classList.toggle("show");
+}
