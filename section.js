2f485f5c7fa3ef8668e662ddf29df8071c11bc22
Keren Skeete <kerenskeete@RBCCM-02s-MacBook-Air.local>
2019-08-07 14:24:19 -0400

    Tips and Tricks on how to care for your Hair

---
+++ section.js
@@ -0,0 +1,31 @@
+// 
+// var image = document.getElementById("changeMe");
+// var origHeight = image.height;
+// var origWidth = image.width;
+//
+// image.onmouseover= function() {
+//   this.style.height = "1000px";
+//   this.style.width = "1000px";
+// };
+//
+// image.onmouseout = function(){
+//   this.style.height = origHeight + "px";
+//   this.style.width = origWidth + "px";
+// };
+//
+// let imgSrcs= ["img/brooklyn-bridge-1.jpg",
+//               "img/ms-halls.jpg",
+//               "img/images.jpg"];
+//
+// let button = document.getElementById("slide");
+// let imageHolder = document.getElementById("changeMe")
+//
+// var i = 0;
+//
+// button.onclick= function(ev){
+//   imageHolder.src = imgSrcs[i];
+//   i++;
+//   if (i > 2) {
+//     i = 0;
+//   }
+// }
